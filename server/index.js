2b4d31c362f3dc00640ff54e31f793d514ac56a2
Randeep Singh <rs513535@gmail.com>
2023-09-17 09:55:35 +0530

    update package

--- server/index.js
+++ server/index.js
@@ -5,15 +5,11 @@ const cors = require('cors');
 const connectDB = require('./mongodb/connect')
 const userRouter = require('./routes/user.routes')
 const propertyRouter = require('./routes/property.routes')
+const port = process.env.PORT || 8080
 
 dotenv.config();
 
 app.use(cors());
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-})
 app.use(express.json({limit: '50mb'}));
 
 app.get('/', (req, res)=>{
@@ -27,7 +23,7 @@ const startServer = async () => {
     try {
         connectDB(process.env.MONGODB_URL);
 
-        app.listen(8080, ()=>{
+        app.listen(port, ()=>{
             console.log('server is started');
         })
     } catch (err) {
